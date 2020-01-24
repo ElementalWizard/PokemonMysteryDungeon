f6a741234357ea55b66f0218a088bdb03e7d7f59
Francisco-Vera <francisco.vera@upr.edu>
2020-01-24 15:32:16 -0400

    Upgraded old Game Engine to be on java 11

---
+++ src/Resources/Images.java
@@ -0,0 +1,66 @@
+package Resources;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by AlexVR on 1/24/2020.
+ */
+public class Images {
+
+
+    public static BufferedImage[] startGameButton;
+    public static BufferedImage titleScreenBackground;
+    public static BufferedImage pauseBackground;
+    public static BufferedImage[] pauseResumeButton;
+    public static BufferedImage[] pauseToTitleButton;
+    public static BufferedImage[] pauseOptionsButton;
+
+    public Images() {
+
+        startGameButton = new BufferedImage[3];
+        pauseResumeButton = new BufferedImage[2];
+        pauseToTitleButton = new BufferedImage[2];
+        pauseOptionsButton = new BufferedImage[2];
+
+
+        try {
+
+            startGameButton[0]= ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Start/NormalStartButton.png"));
+            startGameButton[1]= ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Start/HoverStartButton.png"));
+            startGameButton[2]= ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Start/ClickedStartButton.png"));
+
+            titleScreenBackground = ImageIO.read(getClass().getResourceAsStream("/UI/Backgrounds/Title.png"));
+
+            pauseBackground = ImageIO.read(getClass().getResourceAsStream("/UI/Backgrounds/Pause.png"));
+
+            pauseResumeButton[0] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/Resume/NormalHoverResume.png"));
+            pauseResumeButton[1] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/Resume/PressedResume.png"));
+
+            pauseToTitleButton[0] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/ToTitle/NormalHoverToTitleButton.png"));
+            pauseToTitleButton[1] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/ToTitle/PressedToTitleButton.png"));
+
+            pauseOptionsButton[0] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/ToOptions/NormalHoverToOptionsButton.png"));
+            pauseOptionsButton[1] = ImageIO.read(getClass().getResourceAsStream("/UI/Buttons/Pause/ToOptions/PressedToOptionsButton.png"));
+
+
+
+        }catch (IOException e) {
+        e.printStackTrace();
+    }
+
+
+    }
+
+    public static BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(Images.class.getResourceAsStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+
+}
---
+++ src/Resources/ScoreManager.java
@@ -0,0 +1,16 @@
+package Resources;
+
+import Main.Handler;
+
+/**
+ * Created by AlexVR on 1/24/2020.
+ */
+
+public class ScoreManager {
+
+    Handler handler;
+
+    public ScoreManager(Handler handler) {
+        this.handler = handler;
+    }
+}
